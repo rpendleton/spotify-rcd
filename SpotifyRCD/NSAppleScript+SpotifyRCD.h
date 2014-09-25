cd962ad57e6a053b78df299735a2a61b43dd4e50
Ryan Pendleton <me@ryanp.me>
2014-09-24 18:06:49 -0600

    Initial Commit

---
+++ SpotifyRCD/NSAppleScript+SpotifyRCD.h
@@ -0,0 +1,13 @@
+//
+//  NSAppleScript+SpotifyRCD.h
+//  SpotifyRCD
+//
+//  Created by Ryan Pendleton on 3/24/14.
+//  Copyright (c) 2014 Inline-Studios. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSAppleScript (SpotifyRCD)
+
+@end
---
+++ SpotifyRCD/NSAppleScript+SpotifyRCD.m
@@ -0,0 +1,30 @@
+//
+//  NSAppleScript+SpotifyRCD.m
+//  SpotifyRCD
+//
+//  Created by Ryan Pendleton on 3/24/14.
+//  Copyright (c) 2014 Inline-Studios. All rights reserved.
+//
+
+#import "NSAppleScript+SpotifyRCD.h"
+#import <objc/runtime.h>
+
+@implementation NSAppleScript (SpotifyRCD)
+
++ (void)load
+{
+	unsetenv("DYLD_INSERT_LIBRARIES");
+	[self jr_swizzleMethod:@selector(initWithSource:) withMethod:@selector(is_initWithSource:) error:nil];
+}
+
+- (id)is_initWithSource:(NSString *)source
+{
+	if([source isEqualToString:@"tell application id \"com.apple.iTunes\" to launch"])
+	{
+		source = @"tell application id \"com.spotify.client\" to launch";
+	}
+	
+	return [self is_initWithSource:source];
+}
+
+@end
